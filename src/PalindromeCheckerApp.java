b74d52a5c5b6f1a8e8ffe0bfc0beebe26fe7cdaf
aalif-07 <aalif.g07@gmail.com>
2026-02-27 16:32:26 +0530

    UC2HardcodedPalindrome completed

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,42 @@
-public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("WELCOME TO PALINDROME CHECKER APP ");
-        System.out.println("version 1.0 ");
-        System.out.println("System initialized successfully! ");
-    }
+
+
+        /**
+         * ==================================================================
+         * MAIN CLASS - UseCase2PalindromeCheckerApp
+         * ==================================================================
+         *
+         * Use Case 2: Hardcoded Palindrome Validation
+         *
+         * Description:
+         * This class demonstrates basic palindrome validation
+         * using a hardcoded string value.
+         *
+         * @author Developer
+         * @version 2.0
+         */
+        public class PalindromeCheckerApp {
+
+            public static void main(String[] args) {
+
+                System.out.println("WELCOME TO PALINDROME CHECKER APP ");
+                System.out.println("version 1.0 ");
+                System.out.println("System initialized successfully! ");
+
+                String input = "racecar";
+                boolean isPalindrome = true;
+                int length = input.length();
+
+
+                for (int i = 0; i < length / 2; i++) {
+                    if (input.charAt(i) != input.charAt(length - 1 - i)) {
+                        isPalindrome = false;
+                        break;
+                    }
+                }
+                if (isPalindrome) {
+                    System.out.println("The string \"" + input + "\" is a palindrome.");
+                } else {
+                    System.out.println("The string \"" + input + "\" is NOT a palindrome.");
+                }
+            }
 }
