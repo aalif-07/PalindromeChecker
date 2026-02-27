ec1de1b70578c0c6f993886193fc4b3ec2340220
aalif-07 <mj7841@srmist.edu.in>
2026-02-27 15:01:11 +0530

    updated UC1

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,12 @@
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        /**
+         * Application entry point
+         * this is a java palindrome checker app welcome message
+         * 
+         */
+        System.out.println("WELCOME TO PALINDROME CHECKER APP ");
+        System.out.println("version 1.0 ");
+        System.out.println("System initialized successfully! ");
+    }
+}
