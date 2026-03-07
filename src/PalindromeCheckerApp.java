8d96f98917df3fe36aaf4d9c034faf3d641777e2
AALIF <aalif.g07@gmail.com>
2026-03-08 00:40:08 +0530

    Added UC6 Stack and Queue

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,41 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.Stack;
+
+public class PalindromeCheckerApp {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Input text: ");
+            String input = scanner.nextLine();
+
+            boolean result = isPalindrome(input);
+
+            System.out.println("Is it a Palindrome? : " + result);
+
+            scanner.close();
+        }
+
+        public static boolean isPalindrome(String input) {
+            if (input == null || input.isEmpty()) return false;
+
+            String clean = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+            Stack<Character> stack = new Stack<>();
+            Queue<Character> queue = new LinkedList<>();
+
+            for (char c : clean.toCharArray()) {
+                stack.push(c);
+                queue.add(c);
+            }
+
+            while (!stack.isEmpty()) {
+                if (!stack.pop().equals(queue.poll())) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
